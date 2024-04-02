5db404788dc3cdd32cff86719412b70a52aa47ba
Paul Cleofas <paulcleofas10@gmail.com>
2024-04-02 02:00:16 -0600

    implemented heat levels and route finding

--- graphs/device.py
+++
@@ -1,9 +0,0 @@
-class Device:
-    '''
-        Class to record device information received by a sensor
-        device_address: the MAC address of the device
-        RSSIs: array of RSSIs for a certain device
-    '''
-    def __init__(self, device_address, RSSIs):
-        self.id = device_address
-        self.rssi = RSSIs
