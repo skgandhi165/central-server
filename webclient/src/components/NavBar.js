199cceee12a3ae3e12dd522ab9a45b47b9d59c6b
skgandhi165 <67382333+skgandhi165@users.noreply.github.com>
2024-04-03 15:10:37 -0600

    Update NavBar.js

--- webclient/src/components/NavBar.js
+++ webclient/src/components/NavBar.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
-function NavigationBar() {
+function NavigationBar() {   
+
 return (
     <Navbar bg="dark" variant="dark" expand="lg" >
         <Container>
@@ -10,7 +11,25 @@ return (
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+
+                    <NavDropdown title="FROM : address 1" id = "dropdown-1">
+                        <NavDropdown.Item href = "#address 1">Room 1</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 2">Room 2</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 3">Room 3</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 4">Room 4</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 5">Room 5</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 6">Room 6</NavDropdown.Item>
+                    </NavDropdown>
+
+                    <NavDropdown title="TO : address 2" id = "dropdown-1">
+                        <NavDropdown.Item href = "#address 1">Room 1</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 2">Room 2</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 3">Room 3</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 4">Room 4</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 5">Room 5</NavDropdown.Item>
+                        <NavDropdown.Item href = "#address 6">Room 6</NavDropdown.Item>
+                    </NavDropdown>               
+
                     {/* Add more Nav.Link or NavDropdown components as needed */}
                 </Nav>
             </Navbar.Collapse>
